7e9de87b4114d5b3267f3c4062ce124bdf741935
reshm <reshm@DESKTOP-F10JV1T>
2024-09-05 17:55:13 +0530

    Refactor testcase : Move common setup code outside individual test cases

--- leapyearkata/src/test/java/com/katalearn/leapyearkata/LeapYearTest.java
+++ leapyearkata/src/test/java/com/katalearn/leapyearkata/LeapYearTest.java
@@ -5,51 +5,47 @@ import org.junit.Test;
 
 public class LeapYearTest {
 
+	LeapYear leapyear = new LeapYear();
+
 	@Test
 	public void testIsLeapYear_check400IsALeapYear_returnTrue() {
-		LeapYear leapyear = new LeapYear();
 		boolean result = leapyear.isLeapYear(400);
 		assertTrue(result);
 	}
 
 	@Test
 	public void testIsLeapYear_check2000IsALeapYear_returnTrue() {
-		LeapYear leapyear = new LeapYear();
 		boolean result = leapyear.isLeapYear(2000);
 		assertTrue(result);
 	}
 
 	@Test
 	public void testIsLeapYear_check1001IsALeapYear_returnFalse() {
-		LeapYear leapyear = new LeapYear();
 		boolean result = leapyear.isLeapYear(1001);
 		assertFalse(result);
 	}
-	
+
 	@Test
 	public void testIsLeapYear_check2100IsALeapYear_returnFalse() {
 		LeapYear leapyear = new LeapYear();
 		boolean result = leapyear.isLeapYear(2100);
 		assertFalse(result);
 	}
-	
+
 	@Test
 	public void testIsLeapYear_check2005IsALeapYear_returnFalse() {
-		LeapYear leapyear = new LeapYear();
 		boolean result = leapyear.isLeapYear(2005);
 		assertFalse(result);
 	}
-	
+
 	@Test
 	public void testIsLeapYear_check2008IsALeapYear_returnTrue() {
-		LeapYear leapyear = new LeapYear();
 		boolean result = leapyear.isLeapYear(2008);
 		assertTrue(result);
 	}
-	
+
 	@Test
 	public void testIsLeapYear_check2017IsALeapYear_returnFale() {
-		LeapYear leapyear = new LeapYear();
 		boolean result = leapyear.isLeapYear(2017);
 		assertFalse(result);
 	}
