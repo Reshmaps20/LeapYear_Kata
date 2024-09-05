d113382404ad7320a245fc324e655bf94e40c918
reshm <reshm@DESKTOP-F10JV1T>
2024-09-05 17:34:47 +0530

    TestCase6:2008 is a leap year; divisible by 4,not divisible by 100,400

--- leapyearkata/src/main/java/com/katalearn/leapyearkata/LeapYear.java
+++ leapyearkata/src/main/java/com/katalearn/leapyearkata/LeapYear.java
@@ -8,6 +8,8 @@ public class LeapYear {
 			return true;
 		else if(year%100 ==0)
 			return false;
+		else if(year%4 ==0)
+			return true;
 		else
 			return false;
 	}
--- leapyearkata/src/test/java/com/katalearn/leapyearkata/LeapYearTest.java
+++ leapyearkata/src/test/java/com/katalearn/leapyearkata/LeapYearTest.java
@@ -39,4 +39,11 @@ public class LeapYearTest {
 		boolean result = leapyear.isLeapYear(2005);
 		assertFalse(result);
 	}
+	
+	@Test
+	public void testIsLeapYear_check2008IsALeapYear_returnTrue() {
+		LeapYear leapyear = new LeapYear();
+		boolean result = leapyear.isLeapYear(2008);
+		assertTrue(result);
+	}
 }
